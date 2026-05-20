5ddfee2e46853deb6b86fdcaeeaf97ee59670c53
VulpineFriend87 <vulpinefriend87@gmail.com>
2026-05-20 20:03:26 +0200

    feat: drop versions below 1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,8 @@ version = "1.3"
 description = "SimpleLobby"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
